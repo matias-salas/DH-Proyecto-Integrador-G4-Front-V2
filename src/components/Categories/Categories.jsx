d570dc8706f5ce7e6c29fcc0c084430631c6e8f3
Conrado Els <conradoels@gmail.com>
2023-12-10 03:17:21 -0300

    carousel cat

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import style from './Categories.module.css';
-import baseUrl from '../../utils/baseUrl.json';
-import CategorieSkeleton from '../Skeletons/CategorieSkeleton/CategorieSkeleton';
+import React, { useEffect, useState } from "react";
+import style from "./Categories.module.css";
+import baseUrl from "../../utils/baseUrl.json";
+import CategorieSkeleton from "../Skeletons/CategorieSkeleton/CategorieSkeleton";
+
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const Categories = ({ handleFilterCategories }) => {
   const [categories, setCategories] = useState([]);
@@ -20,20 +33,109 @@ const Categories = ({ handleFilterCategories }) => {
       .catch((err) => console.log(err));
   }, []);
 
-  return (
-    <div className={style.categoriesContainer}>
-      <h2>Buscar por tipo de vehículo</h2>
-      <div className={style.categoriesList}>
-        {isLoading ? (
-          <>
+  // MATERIAL UI
+
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = categories.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);}
+
+    return (
+      <div className={style.categoriesContainer}>
+        <h2>Buscar por tipo de vehículo</h2>
+        <div className={style.categoriesList}>
+          {isLoading ? (
+            <>
+              <CategorieSkeleton />
+              {/* <CategorieSkeleton />
             <CategorieSkeleton />
-            <CategorieSkeleton />
-            <CategorieSkeleton />
-            <CategorieSkeleton />
-          </>
-        ) : (
-          <>
-            {categories.map((category) => (
+            <CategorieSkeleton /> */}
+            </>
+          ) : (
+            <>
+              {
+                <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{categories[activeStep].name}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {categories.map((category, index) => (
+          <div key={category.name}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={category.url}
+                alt={category.name}
+                onClick={() => handleFilterCategories(category.id)}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+
+                /* {categories.map((category) => (
               <div
                 className={style.categoryCard}
                 key={category.id}
@@ -51,12 +153,13 @@ const Categories = ({ handleFilterCategories }) => {
                   </p>
                 </div>
               </div>
-            ))}
-          </>
-        )}
+            ))} */
+              }
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Categories;
