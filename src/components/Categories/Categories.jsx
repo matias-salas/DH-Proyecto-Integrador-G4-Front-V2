76d61a200f4460f66c3bd80f6ecccb02729b1204
Conrado Els <conradoels@gmail.com>
2023-12-10 02:41:17 -0300

    test

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -22,7 +22,7 @@ const Categories = ({ handleFilterCategories }) => {
 
   return (
     <div className={style.categoriesContainer}>
-      <h2>Buscar por tipo de auto</h2>
+      <h2>Buscar por tipo de vehículo</h2>
       <div className={style.categoriesList}>
         {isLoading ? (
           <>
