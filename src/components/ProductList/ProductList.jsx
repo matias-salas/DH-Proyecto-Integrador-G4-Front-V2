341041d8f41e0e9e974a217063bb6673da99ad06
Conrado Els <conradoels@gmail.com>
2023-12-12 02:21:26 -0300

    fetch

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,7 +4,7 @@ import Spinner from "../Spinner/Spinner";
 import style from "./ProductList.module.css";
 import ProductNotFound from "../Product/ProductNotFound";
 
-const ProductList = ({ products, isLoading, notFound }) => {
+const ProductList = ({ products, isLoading }) => {
   //console.log(products);
 
   // sort products by  average_score
@@ -15,89 +15,81 @@ const ProductList = ({ products, isLoading, notFound }) => {
   });
 
   return (
-    <>
-      {notFound ? ( // Verifica si no se encontraron productos
-        <ProductNotFound />
+    <div className={style.container}>
+      {/* <h2 className={style.recommendations}>Recomendaciones</h2> */}
+      {isLoading ? (
+        <div style={{ alignSelf: "center" }}>
+          <Spinner style={{ alignSelf: "center" }} />
+        </div>
       ) : (
-        <div className={style.container}>
-          {/* <h2 className={style.recommendations}>Recomendaciones</h2> */}
-          {isLoading ? (
-            <div style={{ alignSelf: "center" }}>
-              <Spinner style={{ alignSelf: "center" }} />
-            </div>
-          ) : (
-            <>
-              {products.length === 0 ? (
-                /* <p className={style.msgNoProducts}>
+        <>
+          {products.length === 0 ? (
+            /* <p className={style.msgNoProducts}>
               Lamentablemente no hay productos para los filtros seleccionados.
             </p> */ <ProductNotFound />
-              ) : (
-                products.map((product) => (
-                  <Product
-                    key={product.id}
-                    id={product.id}
-                    imgUrl={product.image}
-                    category={product.category}
-                    title={product.name}
-                    description={product.description}
-                    location={product.transmission}
-                    address={product.address}
-                    attributes={product.attributes}
-                    latitude={product.latitude}
-                    longitude={product.longitude}
-                    policiesSite={product.policiesSite}
-                    policiesSecurityAndHealth={
-                      product.policiesSecurityAndHealth
-                    }
-                    policiesCancellation={product.policiesCancellation}
-                    averageScore={product.average_score}
-                    availableDates={[
-                      "2024-01-13",
-                      "2023-11-29",
-                      "2024-01-01",
-                      "2023-11-24",
-                      "2024-01-09",
-                      "2024-01-13",
-                      "2024-01-13",
-                      "2024-01-14",
-                      "2024-01-16",
-                      "2024-01-05",
-                      "2024-01-02",
-                      "2024-01-02",
-                      "2023-11-24",
-                      "2023-11-28",
-                      "2024-01-04",
-                      "2024-01-18",
-                      "2023-11-28",
-                      "2024-01-06",
-                      "2024-01-23",
-                      "2024-01-20",
-                      "2024-01-03",
-                      "2023-11-26",
-                      "2024-01-09",
-                      "2024-01-03",
-                      "2024-01-16",
-                      "2024-01-03",
-                      "2024-01-08",
-                      "2023-11-28",
-                      "2023-11-28",
-                      "2024-01-08",
-                      "2024-01-21",
-                      "2024-01-06",
-                      "2024-01-09",
-                      "2024-01-04",
-                      "2023-11-26",
-                      "2024-01-15",
-                      "2024-01-07",
-                    ]}
-                  />
-                ))
-              )}
-            </>
+          ) : (
+            products.map((product) => (
+              <Product
+                key={product.id}
+                id={product.id}
+                imgUrl={product.image}
+                category={product.category}
+                title={product.name}
+                description={product.description}
+                location={product.transmission}
+                address={product.address}
+                attributes={product.attributes}
+                latitude={product.latitude}
+                longitude={product.longitude}
+                policiesSite={product.policiesSite}
+                policiesSecurityAndHealth={product.policiesSecurityAndHealth}
+                policiesCancellation={product.policiesCancellation}
+                averageScore={product.average_score}
+                availableDates={[
+                  "2024-01-13",
+                  "2023-11-29",
+                  "2024-01-01",
+                  "2023-11-24",
+                  "2024-01-09",
+                  "2024-01-13",
+                  "2024-01-13",
+                  "2024-01-14",
+                  "2024-01-16",
+                  "2024-01-05",
+                  "2024-01-02",
+                  "2024-01-02",
+                  "2023-11-24",
+                  "2023-11-28",
+                  "2024-01-04",
+                  "2024-01-18",
+                  "2023-11-28",
+                  "2024-01-06",
+                  "2024-01-23",
+                  "2024-01-20",
+                  "2024-01-03",
+                  "2023-11-26",
+                  "2024-01-09",
+                  "2024-01-03",
+                  "2024-01-16",
+                  "2024-01-03",
+                  "2024-01-08",
+                  "2023-11-28",
+                  "2023-11-28",
+                  "2024-01-08",
+                  "2024-01-21",
+                  "2024-01-06",
+                  "2024-01-09",
+                  "2024-01-04",
+                  "2023-11-26",
+                  "2024-01-15",
+                  "2024-01-07",
+                ]}
+              />
+            ))
           )}
-        </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
--- src/components/Search/SelectCity/SelectCity.jsx
+++ src/components/Search/SelectCity/SelectCity.jsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCar } from "@fortawesome/free-solid-svg-icons";
 import style from "./SelectCity.module.css";
-import baseUrl from "../../../utils/baseUrl.json";
 
 const SelectCity = ({ enviarDato }) => {
-  const [prod, setProd] = useState([]);
+  // const [prod, setProd] = useState([]);
   const [busqueda, setBusqueda] = useState("");
 
   const handleSearchChange = (e) => {
     const valorBusqueda = e.target.value;
     setBusqueda(valorBusqueda);
+    
+    // const filtered = prod.filter(
+    //   (item) =>
+    //     item.address.toLowerCase().includes(valorBusqueda.toLowerCase()) ||
+    //     item.description.toLowerCase().includes(valorBusqueda.toLowerCase()) ||
+    //     item.name.toString().toLowerCase().includes(valorBusqueda.toLowerCase())
+    // );
 
-    const filtered = prod.filter(
-      (item) =>
-        item.address.toLowerCase().includes(valorBusqueda.toLowerCase()) ||
-        item.description.toLowerCase().includes(valorBusqueda.toLowerCase()) ||
-        item.name.toString().toLowerCase().includes(valorBusqueda.toLowerCase())
-    );
+    //   console.log(filtered);
 
-    console.log(filtered);
-
-    if (filtered.length > 0) {
-      enviarDato(filtered); // Enviar datos filtrados si hay coincidencias
-    } else {
-      enviarDato("zap"); // Enviar un arreglo vacío si no hay coincidencias
-    }
+    //   if (filtered.length > 0) {
+    //     enviarDato(filtered); // Enviar datos filtrados si hay coincidencias
+    //   } else {
+    //     enviarDato("zap"); // Enviar un arreglo vacío si no hay coincidencias
   };
+enviarDato(busqueda);
+  // };
 
-  useEffect(() => {
-    fetch(`${baseUrl.url}/products`)
-      .then((res) => res.json())
-      .then((data) => {
-        setProd(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(`${baseUrl.url}/products`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setProd(data);
+  //     });
+  // }, []);
 
   return (
     <div className={style.selectBox}>
